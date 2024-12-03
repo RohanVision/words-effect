edddfd6f0e6afd3669cb248c4daa5a88bd77813c
Rohan Amberkar <amberkar.rohan17@gmail.com>
2024-12-03 13:50:07 -0500

    Word Falling Effect

---
+++ script.js
@@ -0,0 +1,12 @@
+
+// get all nodes
+const words = document.querySelectorAll(".word");
+
+// Falling Effects;
+
+setTimeout(() => {
+    words.forEach((e) => {
+        e.classList.add("show");
+    })
+}, 1500)
+
